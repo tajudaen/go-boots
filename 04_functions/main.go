2bc2c3d797e1d368976cf09bf75c8adf0d7723c8
tajud99n <giwa208@gmail.com>
2019-09-10 06:58:46 +0100

    practice functions

--- 04_functions/main.go
+++ 04_functions/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func greeting(name string) string {
 	return "Hello " + name
@@ -8,4 +11,31 @@ func greeting(name string) string {
 
 func main() {
 	fmt.Println(greeting("Tajudeen"))
+
+	module := "functions"
+	author := "john doe"
+
+	fmt.Println(converter(module, author))
+
+	bestFinish := bestLeagueFinishes(13, 10, 13, 17, 14, 16)
+	fmt.Println(bestFinish)
+}
+
+func converter(s1, s2 string) (str1, str2 string) {
+	s1 = strings.Title(s1)
+	s2 = strings.ToUpper(s2)
+
+	return s1, s2
+}
+
+//Variadic function
+func bestLeagueFinishes(finishes ...int) int {
+	best := finishes[0]
+
+	for _, position := range finishes {
+		if position < best {
+			best = position
+		}
+	}
+	return best
 }
